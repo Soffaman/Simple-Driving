e520eaed508dfe5d68a1b44928c45db43a6b61ed
Dmytro <112495732+Soffaman@users.noreply.github.com>
2023-08-11 12:02:18 +0300

    Steering The Car formed

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -6,10 +6,19 @@ public class Car : MonoBehaviour
 {
     [SerializeField] private float _speed = 10f;
     [SerializeField] private float _speedGain = 0.2f;
+    [SerializeField] private float _turnSpeed = 200f;
+
+    private int _steerValue;
 
     void Update()
     {
-        transform.Translate(Vector3.forward * _speed * Time.deltaTime);
         _speed += _speedGain * Time.deltaTime;
+        transform.Rotate(0f, _steerValue * _turnSpeed * Time.deltaTime, 0f);
+        transform.Translate(Vector3.forward * _speed * Time.deltaTime);
     }
+
+    public void Steer(int value)
+    {
+        _steerValue = value;
+    } 
 }
